8e14c806f04ce081ee0407a5e51f457cf4d4cfbe
stargoo <stargoo@users.noreply.github.com>
2017-12-09 16:38:00 -0800

    Update .cdwin.zsh

--- .cdwin.zsh
+++ .cdwin.zsh
@@ -1,11 +1,20 @@
 function cdwin(){
     drive=${1:0:1}
-    if [[ $drive =~ "m" || $drive =~ "M" ]]; then
-        line=$(sed -e 's#^[Mm]:#/home/scott/mdrive#' -e 's#\\#/#g' <<< "$1")
-    elif [[ $drive =~ "u" || $drive =~ "U" ]]; then
-        line=$(sed -e 's#^[Uu]:#/home/scott/udrive#' -e 's#\\#/#g' <<< "$1")
+    elif [[ $drive =~ "r" || $drive =~ "R" ]]; then
+        line=$(sed -e 's#^[Rr]:#/home/sie/rdrive#' -e 's#\\#/#g' <<< "$1")
     elif [[ $drive =~ "s" || $drive =~ "S" ]]; then
-        line=$(sed -e 's#^[Ss]:#/home/scott/sdrive#' -e 's#\\#/#g' <<< "$1")
+        line=$(sed -e 's#^[Ss]:#/home/sie/sdrive#' -e 's#\\#/#g' <<< "$1")
+    fi
+    print "change directory to $line"
+    cd "$line"
+}
+
+function wincd(){
+    drive=${1:0:1}
+    elif [[ $drive =~ "r" || $drive =~ "R" ]]; then
+        line=$(sed -e 's#^[Rr]:#/home/sie/rdrive#' -e 's#\\#/#g' <<< "$1")
+    elif [[ $drive =~ "s" || $drive =~ "S" ]]; then
+        line=$(sed -e 's#^[Ss]:#/home/sie/sdrive#' -e 's#\\#/#g' <<< "$1")
     fi
     print "change directory to $line"
     cd "$line"
